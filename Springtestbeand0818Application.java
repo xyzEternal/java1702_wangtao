8f4f9aa86b5ec6550311e2d8d58b2843a70a2335
xyzEternal <1658333927@qq.com>
2017-08-18 20:04:35 +0800

    Springtestbeand0818Application.java

--- Springtestbeand0818Application.java
+++ Springtestbeand0818Application.java
@@ -6,7 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springtestbeand0818Application {
 
+	public  void dq(String[] args) {
+		SpringApplication.run(Springtestbeand0818Application.class, args);
+	}
+
+
+	public void sq(String[] args) {
+		SpringApplication.run(Springtestbeand0818Application.class, args);
+	}
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(Springtestbeand0818Application.class, args);
+		
 	}
 }
