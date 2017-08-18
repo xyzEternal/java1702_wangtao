29bbf2c39456c0c3d042d5a64a8464666156ac45
xyzEternal <1658333927@qq.com>
2017-08-18 19:57:25 +0800

    Springtestbeand0818Application.java

---
+++ Springtestbeand0818Application.java
@@ -0,0 +1,12 @@
+package com.springbean.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springtestbeand0818Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springtestbeand0818Application.class, args);
+	}
+}
